d4b649f44eed6c0b6d077fe46835ce60861d799b
Leonardo <leonardo.lima.luz1@gmail.com>
2021-09-23 17:28:13 -0400

    23-09-2021

---
+++ Cursos/Rocketseat/Discover/Guias estelares/o guia estelar de JavaScrip/praticas avançadas/script.js
@@ -0,0 +1,9 @@
+// Object
+
+const person = {
+  name: 'jhon',
+  age: 30,
+  weight: 88.6,
+  aiAdmin: true,
+};
+console.log(person);
---
+++ Cursos/Rocketseat/Discover/Guias estelares/o guia estelar de JavaScrip/praticas avançadas/script1.js
@@ -0,0 +1,13 @@
+// Array
+
+const animals = [
+  'Lion',
+  'Monkey',
+  {
+    name: 'Cat',
+    age: 3,
+  },
+];
+
+// acessar valores dentro do Array
+console.log(animals);
