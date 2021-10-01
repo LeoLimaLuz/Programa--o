e52b40091a5ed27477787188930a69d6409ab768
Leonardo <leonardo.lima.luz1@gmail.com>
2021-10-01 16:14:11 -0400

    01-10-2021

---
+++ Cursos/Rocketseat/Discover/Guias estelares/o guia estelar de JavaScrip/manipulando dados/script copy 29.js
@@ -0,0 +1,18 @@
+/* ### Celsius em fahrenheit 
+crie uma função que receba uma string em Ccelsius ou 
+fahrenheit e faça a tranformação  de uma unidade para outra
+
+c = (f - 32) * 5/9
+
+F = * 9/5 + 32
+
+*/
+
+// tranform('50F')
+function tranformDegree(degree) {
+  const celsiusEcxists = degree.toUpperCase().includes('C');
+  const fahrenheit = degree.toUpperCase().includes('F');
+
+  if (!celsiusEcxists && !fahrenheitExists) {
+  }
+}
--- Cursos/Rocketseat/Discover/Guias estelares/o guia estelar de JavaScrip/manipulando dados/script.js
+++ Cursos/Rocketseat/Discover/Guias estelares/o guia estelar de JavaScrip/manipulando dados/script.js
@@ -1,18 +1,73 @@
-/* ### Celsius em fahrenheit 
-crie uma função que receba uma string em Ccelsius ou 
-fahrenheit e faça a tranformação  de uma unidade para outra
+/*
+   buscando e contando dados em Array 
 
-c = (f - 32) * 5/9
+   buscando no Array de livros por Categoria abaixo, faço os seguntes desafios 
 
-F = * 9/5 + 32
+    ° contar o Números de categorias e o número de livros em cada categoria
+    ° Contar o número de autores
+    ° Mostraar livros do autor Augusto Cury
+    ° Transformar a função acima em uma função que irá receber o nome do autor e 
+    devolver os livros desse autor.
 
-*/
+    */
 
-// tranform('50F')
-function tranformDegree(degree) {
-  const celsiusEcxists = degree.toUpperCase().includes('C');
-  const fahrenheit = degree.toUpperCase().includes('F');
+const booksByCategory = [
+  {
+    category: 'Riqueza',
+    books: [
+    {
+      title: "Os segredos da mente milionária"
+      author: "George s. Clason",
+    }  
+    {
+      title: "O homen mais rico da Babilõnia",
+      author: "George S. Clason",
+    }
+    {
+      title "Pai rico, pai pobre",
+      author: "Robert T. Kiyosaki e Sharon L. Lechter",
+    }
+   
 
-  if (!celsiusEcxists && !fahrenheitExists) {
+ ];
+ },
+ {
+   category: "Inteligência Emocional"
+   books: [
+
+   {
+
+   title: "Você é Insubstituível",
+   author: "Augusto Cury",
+  } 
+  {
+    title: "Ansiedade - Como enfrentar o mal do século",
+    author: "Augusto Cury",
   }
+  {
+    title: "Os 7 hábitos das pessoas altamente eficazes"
+    author: "Stephen R. Covery",
+  }
+]
+ }
+];
+const totalCategories = booksByCategory.length
+
+console.log(totalCategoris);
+for(let category of booksByCategory) {
+  console.log('Total de livros da categoria: ' category.category)
+  console.log(category.books.length)
+}
+
+function countAuthors() {
+  let authors = []
+  
+  for(let book of category.books) {
+    if(authors.index0f(book.authors) == - 1) {
+      authors.push(book.title)
+     
+    }
+    } 
+  }
+  console.log("Total de autores: ", authors.length )
 }
